0b3bcc771c50146681ed31d4c2cca6ac012e4096
HuangQiuRui <3045696929@qq.com>
2023-05-21 21:14:00 +0800

    验证码

--- php/login.php
+++ php/login.php
@@ -13,47 +13,24 @@ $conn->select_db("art");
 if (isset($_POST['username'])&&isset($_POST['password'])){//登录表单已提交
     
     //获取用户输入的验证码
-    // $captcha = isset($_POST['captcha']) ? trim($_POST['captcha']) : '';
-    // //获取Session中的验证码
-    // session_start();
-    // if(empty($_SESSION['captcha'])){  //如果Session中不存在验证码，则退出
-    //     exit('验证码已经过期，请返回并刷新页面重试。');
-    // }
+    $captcha = isset($_POST['captcha']) ? trim($_POST['captcha']) : '';
+    //获取Session中的验证码
+    session_start();
+    if(empty($_SESSION['captcha'])){  //如果Session中不存在验证码，则退出
+        exit('验证码已经过期，请返回并刷新页面重试。');
+    }
     //获取验证码并清除Session中的验证码
-    // $true_captcha = $_SESSION['captcha'];
-    // unset($_SESSION['captcha']); //限制验证码只能验证一次，防止重复利用
-    // //忽略字符串的大小写，进行比较
-    // if(strtolower($captcha) !== strtolower($true_captcha)){
-    //     exit('您输入的验证码不正确！请返回并刷新页面重试。');
-    // }
+    $true_captcha = $_SESSION['captcha'];
+    unset($_SESSION['captcha']); //限制验证码只能验证一次，防止重复利用
+    //忽略字符串的大小写，进行比较
+    if(strtolower($captcha) !== strtolower($true_captcha)){
+        exit('您输入的验证码不正确！请返回并刷新页面重试。');
+    }
     //验证码验证通过，继续判断用户名和密码
-
-    //获取用户输入的用户名密码
-    // $username=$_POST["username"];
-    // $password=$_POST["password"];
-
-    // $sql = "SELECT * FROM user";
-    // $result = $conn->query($sql);
-    // $rows = $result->fetch_array();
-    // if($rows) {
-    //     //拿着提交过来的用户名和密码去数据库查找，看是否存在此用户名以及其密码
-    //       if ($username == $rows["username"] && $password == $rows["password"]) {
-    //         //echo "验证成功！<br>";
-    //         session_start();
-    //         $_SESSION['username']=$_POST['username'];
-    //         echo "<script>alert('欢迎');location='../html/main.html'</script>";
-    //       } else {
-    //         //echo "用户名或者密码错误<br>";
-    //         echo "<script>alert('账号不存在或密码错误，点击前往注册');location='../html/register.html'</script>";
-    //         //echo "<a href='login.html'>返回</a>";
-    //       }
-    //   }
 
     // 设置参数值
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    //TODO: salt
 
     // 创建预处理语句
     $stmt = $conn->prepare("SELECT * FROM user WHERE username = ?");
--- php/register.php
+++ php/register.php
@@ -9,6 +9,22 @@ $conn->select_db("art");
 
 if (isset($_POST['username'])&&isset($_POST['password'])){
 
+    //获取用户输入的验证码
+    $captcha = isset($_POST['captcha']) ? trim($_POST['captcha']) : '';
+    //获取Session中的验证码
+    session_start();
+    if(empty($_SESSION['captcha'])){  //如果Session中不存在验证码，则退出
+        exit('验证码已经过期，请返回并刷新页面重试。');
+    }
+    //获取验证码并清除Session中的验证码
+    $true_captcha = $_SESSION['captcha'];
+    unset($_SESSION['captcha']); //限制验证码只能验证一次，防止重复利用
+    //忽略字符串的大小写，进行比较
+    if(strtolower($captcha) !== strtolower($true_captcha)){
+        exit('您输入的验证码不正确！请返回并刷新页面重试。');
+    }
+    //验证码验证通过，继续判断用户名和密码
+
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = isset($_POST['email'])?$_POST['email']:NULL;
