997be5cb7a4c137ab767d5e0c9aa259aff8a50d7
HuangQiuRui <3045696929@qq.com>
2023-05-28 12:01:53 +0800

    图片缩放裁剪

--- php/img_test.php
+++ php/img_test.php
@@ -2,8 +2,6 @@
 if (isset($_POST['imgName'])){
     $filename = $_POST["imgName"];
     $base64 = $_POST["base64"];
-    //echo $base64;
-    //echo json_encode($base64);
 
     if(strstr($base64, ',')){
         $base64 = explode(',', $base64);
@@ -14,6 +12,73 @@ if (isset($_POST['imgName'])){
     $fp= fopen("../image/".$filename, "w");
     $len = fwrite($fp, base64_decode($base64));
     fclose($fp);
-    echo $len .'字节被写入了\n';
 
+    // 缩放 large
+    $source = imagecreatefromjpeg("../image/".$filename);
+    list($width, $height) = getimagesize("../image/".$filename);
+    $height = imagesy($source);
+    $newheight = 900;
+    $newwidth = $width * 900 / $height;
+    // Load
+    $target900 = imagecreatetruecolor($newwidth, $newheight);    
+    
+    imagecopyresampled($target900, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+    // Output imagejpeg($target);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target900,'../image/large/'.$filename);
+    imagedestroy($target900);
+
+    // medium
+    $newheight = 500;
+    $newwidth = $width * 500 / $height;
+    $target500 = imagecreatetruecolor($newwidth, $newheight);    
+    imagecopyresampled($target500, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target500,'../image/medium/'.$filename);
+    imagedestroy($target500);
+
+    // small
+    $newheight = 200;
+    $newwidth = $width * 200 / $height;
+    $target200 = imagecreatetruecolor($newwidth, $newheight);    
+    imagecopyresampled($target200, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target200,'../image/small/'.$filename);
+    imagedestroy($target200);
+
+    // 裁剪正方形
+    $short = min($width,$height);
+    $delHeight = ($height-$short) / 2;
+    $delWidth = ($width-$short) / 2;
+    $square = imagecreatetruecolor($short, $short);    
+    imagecopy($square, $source, 0, 0, $delWidth, $delHeight, $width, $height);
+    header('Content-Type: image/jpeg');
+    //imagejpeg($square,'../image/'.'d.jpg');
+    //imagedestroy($square);    
+
+    // medium
+    $side = 150;
+    $target = imagecreatetruecolor($side, $side);    
+    imagecopyresampled($target, $square, 0, 0, 0, 0, $side, $side, $short, $short);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target,'../image/square-medium/'.$filename);
+    imagedestroy($target);
+    // small
+    $side = 75;
+    $target = imagecreatetruecolor($side, $side);    
+    imagecopyresampled($target, $square, 0, 0, 0, 0, $side, $side, $short, $short);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target,'../image/square-small/'.$filename);
+    imagedestroy($target);
+    // tiny
+    $side = 35;
+    $target = imagecreatetruecolor($side, $side);    
+    imagecopyresampled($target, $square, 0, 0, 0, 0, $side, $side, $short, $short);
+    header('Content-Type: image/jpeg');
+    imagejpeg($target,'../image/square-tiny/'.$filename);
+    imagedestroy($target);
+
+    //echo $len .'字节被写入了\n';
+    // TODO: 删除Image路径下原图
+    unlink("../image/".$filename);
 }
